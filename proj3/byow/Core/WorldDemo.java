246092b426996c21b2963369ed4a6df86747156a
Carlos Rodriguez-Raya <carodriguezray@berkeley.edu>
2022-11-22 07:18:48 -0800

    simplified previously added methods

--- proj3/byow/Core/WorldDemo.java
+++ proj3/byow/Core/WorldDemo.java
@@ -11,6 +11,7 @@ import java.util.*;
 import java.util.List;
 
 import static java.lang.Math.abs;
+import static java.lang.Math.floor;
 
 public class WorldDemo {
     public static final int WIDTH = 80;
@@ -87,23 +88,10 @@ public class WorldDemo {
 
         }
 
-        /** I was attempting to see if I could find a very simple way of connecting rooms, and the following code does
-         * an okay job at creating connections. For each rectangle that we define as a room, we take a random rectangle out
-         * of our array and connect the two. Thus, every room will have a connection. Very stupid, I know! Uncomment and run main
-         * if you'd like to see the result. Must fix edge cases, there are no walls around some corners. This is an error within
-         * the linearPath method.*/
+        int index = random.nextInt(rooms.size() + 1);
+        for (Rectangle rec : rooms) {
 
-        // int index = random.nextInt(0, rooms.size() + 1);
-        // for (Rectangle rec : rooms) {
-        //   createLinearPath(tiles, (int) rec.getCenterX(), (int) rec.getCenterY(),
-        //            (int) rooms.get(index).getCenterX(), (int) rooms.get(index).getCenterY(), random);
-        //}
-
-        /** If you'd like to see what createLinearPath generates given parameters, try uncommenting the following examples:*/
-
-        // createLinearPath(tiles, 20, 10, 50, 10, random);
-        // createLinearPath(tiles, 20, 5, 50, 20, random);
-
+        }
     }
 
 
@@ -112,61 +100,74 @@ public class WorldDemo {
      * diligently to create something more elegant.*/
     public static void createLinearPath(TETile[][] tiles, int fromX, int fromY, int destinationX, int destinationY, Random random) {
         if (fromX > destinationX) {
-            for (int x = fromX; x != destinationX; x--) {
-                for (int count = -1; count != 2; count++) {
-                    if (fromY+count < HEIGHT || fromY+count > 0) {
-                        if (tiles[x][fromY+count] != Tileset.FLOOR) {
-                            tiles[x][fromY+count] = Tileset.WALL;
-                        }
-                    }
-                }
-                tiles[x][fromY] = Tileset.FLOOR;
-            }
+            generateHallwayTiles(tiles, fromX, fromY, destinationX, destinationY, true);
         } else {
-            for (int x = fromX; x != destinationX; x++) {
-                for (int count = -1; count != 2; count++) {
-                    if (fromY+count > HEIGHT || fromY+count > 0) {
-                        if (tiles[x][fromY+count] != Tileset.FLOOR) {
-                            tiles[x][fromY+count] = Tileset.WALL;
-                        }
-                    }
-                }
-                tiles[x][fromY] = Tileset.FLOOR;
-            }
+            generateHallwayTiles(tiles, destinationX, fromY, fromX, destinationY, true);
         }
         if (fromY > destinationY) {
+            generateHallwayTiles(tiles, fromX, fromY, destinationX, destinationY, false);
+        } else {
+            generateHallwayTiles(tiles, fromX, destinationY, destinationX, fromY, false);
+        }
+    }
+
+    public static void createHallwayEdgesX(TETile[][] tiles, int x, int destinationX, int fromX, int fromY, int adjust) {
+        if (tiles[x - adjust][fromY] != Tileset.FLOOR) {
+            tiles[x - adjust][fromY] = Tileset.WALL;
+            for (int count = -1; count != 2; count++) {
+                if (tiles[x - adjust][fromY + count] != Tileset.FLOOR) {
+                    tiles[x - adjust][fromY + count] = Tileset.WALL;
+                }
+            }
+        }
+    }
+
+    public static void createHallwayEdgesY(TETile[][] tiles, int y, int destinationY, int fromX, int adjust) {
+        if (tiles[fromX][y - adjust] != Tileset.FLOOR) {
+            tiles[fromX][y - adjust] = Tileset.WALL;
+            for (int count = -1; count != 2; count++) {
+                if (tiles[fromX + count][y - adjust] != Tileset.FLOOR) {
+                    tiles[fromX + count][y - adjust] = Tileset.WALL;
+                }
+            }
+        }
+    }
+
+    public static void generateHallwayTiles(TETile[][] tiles, int fromX, int fromY, int destinationX, int destinationY, boolean axis) {
+        if (axis) {
+            for (int x = fromX; x != destinationX; x--) {
+                for (int count = -1; count != 2; count++) {
+                    if (tiles[x][fromY + count] != Tileset.FLOOR) {
+                        tiles[x][fromY + count] = Tileset.WALL;
+                    }
+                }
+                tiles[x][fromY] = Tileset.FLOOR;
+                if (x == destinationX + 1) {
+                    createHallwayEdgesX(tiles, x, destinationX, fromX, fromY, 1);
+                } else if (x == fromX) {
+                    createHallwayEdgesX(tiles, x, destinationX, fromX, fromY, -1);
+                }
+
+            }
+        } else {
+            destinationY -= 1;
             for (int y = fromY; y != destinationY; y--) {
                 for (int count = -1; count != 2; count++) {
-                    if (fromX+count < WIDTH || fromX+count > 0) {
-                        if (tiles[fromX+count][y] != Tileset.FLOOR) {
-                            tiles[fromX+count][y] = Tileset.WALL;
+                    if (fromX + count < WIDTH || fromX + count > 0) {
+                        if (tiles[fromX + count][y] != Tileset.FLOOR) {
+                            tiles[fromX + count][y] = Tileset.WALL;
                         }
                     }
                 }
-                tiles[fromX][y] = Tileset.FLOOR;
-            }
-        } else {
-            for (int y = fromY; y != destinationY; y++) {
-                for (int count = -1; count != 2; count++) {
-                    if (fromX+count < WIDTH || fromX+count > 0) {
-                        if (tiles[fromX+count][y] != Tileset.FLOOR) {
-                            tiles[fromX+count][y] = Tileset.WALL;
-                        }
-                    }
+                if (y == destinationY + 1) {
+                    createHallwayEdgesY(tiles, y, destinationY, fromX, 1);
+                } else if (y == fromY) {
+                    createHallwayEdgesY(tiles, y, destinationY, fromX, -1);
                 }
                 tiles[fromX][y] = Tileset.FLOOR;
             }
         }
     }
-
-    /** Started creating a path method that does not generate a boring pathQ*/
-    public static void createPath(TETile[][] tiles, int fromX, int fromY, int destinationX, int destinationY, Random random) {
-        int distanceX = abs(destinationX - fromX);
-        int pathLength;
-        int coinFlip;
-    }
-
-
 
     public static void main(String[] args) {
 
@@ -177,7 +178,7 @@ public class WorldDemo {
          * The world should generate a random number of rooms between 5 and 25 of many. The rooms should
          * be of a random size within given parameters. Note that if you put the same seed in, you get
          * the same world generation! That will be useful later.*/
-        myWorldDemo = engine.interactWithInputCheese("831");
+        myWorldDemo = engine.interactWithInputCheese("123");
         ter.renderFrame(myWorldDemo);
     }
 
